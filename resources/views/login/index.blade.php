d040907a430dd862aebb26a05dd82383ac67f06e
fahrilhadi <fahrilhadi77@gmail.com>
2023-09-22 15:29:53 +0700

    revisi ui login v1.0

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.98.0">
-    <title>Login | SITEI</title>
+    <title>SITEI | Universitas Riau</title>
 
   
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
@@ -20,16 +20,10 @@
 
   <body>
 
-    <div class="container overflow-hidden text-center">
-  <div class="masuk row gx-5">
-    <div class="col-lg mt-5">
-     <div class=" gambar p-3 mt-5">
-      <img src="assets/dist/img/unri.png" alt="" width="250">
-      <h2 class=" mt-5 fw-bold">Sistem Informasi <br>Teknik Elektro dan Informatika</h2>
-     </div>
-    </div>
-    <div class="col mt-5">
-      <div class="kotak-masuk p-3 mt-5"><main class="form-signin w-100 m-auto">
+<div class="container overflow-hidden text-center">
+  <div class="masuk row">
+    <div class="col">
+      <div class="kotak-masuk p-3"><main class="form-signin w-100 m-auto">
       @if (session()->has('loginError'))
       <div class="alert alert-danger alert-dismissible fade show float-left" role="alert">
         {{session('loginError')}}
@@ -39,12 +33,15 @@
 
         <form class="form-login" action="/" method="POST" class="text-center mt-5">
           @csrf
-          
-            <h1 class=" h3 mb-4 fw-bold">Selamat Datang!</h1>
+
+            <div class="gambar">
+              <img src="assets/dist/img/unri.png" alt="logo_unri" width="105" height="106">
+              <h4>Sistem Informasi <br>Teknik Elektro dan Informatika</h4>
+           </div>
 
             <div class="form-floating mt-5">
-            <input type="text" class="form-control @error('username') is-invalid @enderror" name="username" id="username" value="{{ old('username') }}" placeholder="username" autofocus required>
-            <label class="label-nim" for="username">NIP/NIM</label>
+              <input type="text" class="form-control rounded-1 @error('username') is-invalid @enderror" name="username" id="username" value="{{ old('username') }}" placeholder="username" autofocus required>
+              <label class="label-nim" for="username">NIP/NIM <span class="text-danger">*</span></label>
             @error('username')
                 <div class="invalid-feedback">
                   {{$message}}
@@ -52,19 +49,27 @@
             @enderror
             </div>
             
-            <div class="form-floating">
-            <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
-            <label for="password">Password</label>
+            <div class="form-floating mt-3">
+            <input type="password" class="form-control rounded-1" name="password" id="password" placeholder="Password" required>
+            <label for="password">Password <span class="text-danger">*</span></label>
             </div>
             
-            <button class="mt-4 w-100 btn btn-lg btn-success rounded-pill border" type="submit">Log in</button>
+            <button class="w-100 btn btn-lg btn-login mt-4 rounded-1" type="submit">Masuk</button>
         </form>
-        <small class=" kecil d-block text-center mt-3">Belum Terdaftar? <b>Hubungi Staff Jurusan!</b></small>
+        <small class="kecil d-block text-center mt-4">Belum memiliki akun? <br>Silahkan hubungi Admin Prodi<br></small>
     </main></div>
     </div>
-    <div class="footer-login mt-5">
-  <strong>Dikembangkan oleh Prodi Teknik Informatika UNRI</strong>
-</div>
+      {{-- <div class="col d-flex justify-content-center align-items-center">
+        <div class="col">
+          <div class="text-center my-3"><a class="btn btn-success text-white rounded-1" href="#">SIA JTE <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" class="ms-1 mb-1" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z"></path></svg></a></div>
+        </div>
+        <div class="col">
+          <div class="text-center my-3"><a class="btn btn-success text-white rounded-1" href="#">SIA JTE <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" class="ms-1 mb-1" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z"></path></svg></a></div>
+        </div>
+      </div> --}}
+    <div class="mt-5">
+      <h6>Dikembangkan oleh Prodi Teknik Informatika</h6>
+    </div>
   </div>  
 </div>
 
