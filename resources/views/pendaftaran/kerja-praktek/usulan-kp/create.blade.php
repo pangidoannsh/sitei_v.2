b2ad10c994518733a4eb54ee7c43fb15c6f047d4
naldimuhammad <seprinaldimuhammad507@gmail.com>
2023-10-02 16:35:58 +0700

    update view create usulan kp

--- resources/views/pendaftaran/kerja-praktek/usulan-kp/create.blade.php
+++ resources/views/pendaftaran/kerja-praktek/usulan-kp/create.blade.php
@@ -23,7 +23,7 @@
 
 <div class="container ">
     <div class="mb-2 field">
-            <label class="form-label mb-2 ">Perusahaan/Instansi</label>
+            <label class="form-label mb-2 ">Perusahaan/Instansi<span class="text-danger">*</span></label>
             <input type="text" name="nama_perusahaan"  class="form-control @error ('nama_perusahaan') is-invalid @enderror" value="{{ old('nama_perusahaan') }}" required autofocus>
             @error('nama_perusahaan')
               <div class="invalid-feedback">
@@ -33,7 +33,7 @@
         </div>  
 
         <div class="mb-2 field">
-            <label class="form-label mb-2">Alamat Perusahaan</label>
+            <label class="form-label mb-2">Alamat Perusahaan<span class="text-danger">*</span></label>
             <input type="text" name="alamat_perusahaan" class="form-control @error('alamat_perusahaan') is-invalid @enderror" value="{{ old('alamat_perusahaan') }}" required>
             @error('alamat_perusahaan')
               <div class="invalid-feedback">
@@ -42,7 +42,7 @@
             @enderror
         </div>
         <div class="mb-2 field">
-            <label class="form-label mb-2">Bidang Usaha/Kegiatan</label>
+            <label class="form-label mb-2">Bidang Usaha/Kegiatan<span class="text-danger">*</span></label>
             <input type="text" name="bidang_usaha" class="form-control @error('bidang_usaha') is-invalid @enderror" value="{{ old('bidang_usaha') }}" required>
             @error('bidang_usaha')
               <div class="invalid-feedback">
@@ -52,7 +52,7 @@
         </div>
 
             <div class="mb-2">
-            <label for="formFile" class="form-label mb-2 float-start">KRS Semester Berjalan <small class="text-secondary">( Format .pdf .jpeg .png .jpg | Maks. 200 KB ) </small></label> 
+            <label for="formFile" class="form-label mb-2 float-start">KRS Semester Berjalan<span class="text-danger">*</span> <small class="text-secondary">( Format .pdf | Maks. 200 KB ) </small></label> 
             <input name="krs_berjalan" class="form-control @error ('krs_berjalan') is-invalid @enderror" value="{{ old('krs_berjalan') }}" type="file" id="formFile" required >
 <!-- old tidak bisa di pakai disini karna pertimbangan security, jgn sampai org tau directory kita -->
             @error('krs_berjalan')
@@ -62,7 +62,7 @@
             @enderror
     </div>
     <div class="mb-2">
-            <label for="formFile" class="form-label mb-2 float-start">Transkip Nilai <small class="text-secondary">( Format .pdf .jpeg .png .jpg | Maks. 200 KB ) </small></label>
+            <label for="formFile" class="form-label mb-2 float-start">Transkip Nilai<span class="text-danger">*</span> <small class="text-secondary">( Format .pdf | Maks. 200 KB ) </small></label>
             <input name="transkip_nilai" class="form-control @error ('transkip_nilai') is-invalid @enderror" value="{{ old('transkip_nilai') }}" type="file" id="formFile" required>
 
             @error('transkip_nilai')
@@ -73,9 +73,9 @@
     </div>
 
        <div class="mb-2 field">
-            <label for="dosen_pembimbing_nip" class="form-label mb-2">Dosen Pembimbing</label>
+            <label for="dosen_pembimbing_nip" class="form-label mb-2">Dosen Pembimbing<span class="text-danger">*</span></label>
             <select name="dosen_pembimbing_nip" id="pembimbing_nip" class="form-select @error('dosen_pembimbing_nip') is-invalid @enderror" required>
-                <option value="">-Pilih-</option>
+                <option value="">-Pilih Pembimbing-</option>
                 @foreach ($dosens as $dosen)
                     <option value="{{$dosen->nip}}" {{old('dosen_pembimbing_nip') == $dosen->nama ? 'selected' : null}}>{{$dosen->nama}}</option>
                 @endforeach
@@ -86,8 +86,9 @@
             </div>
             @enderror
         </div>
+
 <div class="mb-2 field">
-            <label class="form-label mb-2">Rencana mulai KP</label>
+            <label class="form-label mb-2">Rencana mulai KP<span class="text-danger">*</span></label>
             <input type="date" name="tanggal_rencana" class="form-control @error('tanggal_rencana') is-invalid @enderror" value="{{ old('tanggal_rencana') }}" required>
             @error('tanggal_rencana')
               <div class="invalid-feedback">
